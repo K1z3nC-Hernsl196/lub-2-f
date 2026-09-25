14091f3cf605ddca21de1ffe85e7d069ae25b9c7
Anna Shmakova <a_shmakova2007@mail.ru>
2026-09-25 19:59:31 +0600

    Первый коммит

---
+++ main.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <windows.h>
+//подключаем инструменты для работы с вводом и выводом далее MAIN начало программы
+#define INV_SIZE 10
+int main()
+{
+	SetConsoleOutputCP(CP_UTF8);
+	SetConsoleCP(CP_UTF8);
+
+	int current_day = 1;
+	int current_hour = 8;
+
+	int running = 1;
+	// создадим целочисл. переменную (для выбора игрока в меню)
+	int choice;
+
+	int inventory[INV_SIZE] = { 0 };
+	//создаем массив из 10 эл. и chair хранит символы в адрес.строке
+	// * указывает на строку символов
+	char* item_names[10] = {
+		"Пусто",
+		"Дерево",
+		"Камень",
+		"Семена",
+		"Морковка",
+		"Ведро воды",
+		"Земля",
+		"Рыба",
+		"Удочка",
+		"Лопата"
+	};
+
+	inventory[0] = 1;
+	inventory[1] = 2;
+	inventory[2] = 3;
+	inventory[3] = 4;
+	inventory[4] = 5;
+	inventory[5] = 6;
+	inventory[6] = 7;
+	inventory[7] = 8;
+	inventory[8] = 9;
+	inventory[9] = 0;
+
+	while (running)
+	{
+		printf("\n===== МЕНЮ ФЕРМЕРА ====\n"
+			"[0] Выход\n"
+			"[1] Посмотреть на часы\n"
+			"[2] Перемотать время(поработать)\n"
+			"[3] Посмотреть инвентарь\n"
+			"[4] Положить предмет в слот\n"
+			"[5] Выбросить предмет\n"
+			"[6] Выполнить задание по варианту:\n"
+			"Выберите действие: ");
+
+		scanf("%d", &choice);
+
+		switch (choice)
+		{
+		case 0:
+			running = 0;
+			break;
+
+		}
+	}
+	return 0;
+}
