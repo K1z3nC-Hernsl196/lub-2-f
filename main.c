45be86bca2c8e74db11842cfa024b8c1463ff4a3
Anna Shmakova <a_shmakova2007@mail.ru>
2026-09-25 21:08:26 +0600

    Второй коммит: добавление действий в меню

--- main.c
+++ main.c
@@ -52,7 +52,7 @@ int main()
 			"[4] Положить предмет в слот\n"
 			"[5] Выбросить предмет\n"
 			"[6] Выполнить задание по варианту:\n"
-			"Выберите действие: ");
+			"Выберите действие: \n");
 
 		scanf("%d", &choice);
 
@@ -61,6 +61,55 @@ int main()
 		case 0:
 			running = 0;
 			break;
+
+		case 1:
+			printf("Текущее время: День %d, %02d:00\n", current_day, current_hour);
+			break;
+
+		case 2:
+		{
+			int hours;
+			printf("Сколько часов вы хотите потратить на работу? ");
+			scanf("%d", &hours);
+			current_hour += hours;
+			while (current_hour >= 24)
+			{
+				current_hour -= 24;
+				current_day++;
+			}
+			printf("Новое время: День %d, %02d:00\n", current_day, current_hour);
+		}
+		break;
+
+		case 3:
+			for (int i = 0; i < INV_SIZE; i++)
+			{
+				printf("Слот %d: [%d] ", i, inventory[i]);
+				printf("(%s)\n", item_names[inventory[i]]);
+			}
+			break;
+
+		case 4: {
+			int slot;
+			int item_id;
+			printf("Введите номер слота(0-9): \n");
+			scanf("%d", &slot);
+			if (slot < 0 || slot >= INV_SIZE)
+			{
+				printf("Такого слота нет (-_-), меняй \n");
+				break;
+			}
+			printf("Введите ID предмета: \n");
+			scanf("%d", &item_id);
+			if (item_id < 0 || item_id >= INV_SIZE)
+			{
+				printf("Такого ID нет (-_-),меняй \n");
+				break;
+			}
+			inventory[slot] = item_id;
+			printf("Предмет %d помещен в слот %d\n", slot);
+			break;
+		}
 
 		}
 	}
